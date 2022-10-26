5cd46d61b270c9fcc56cc76dc9a0035cbe23d166
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-10-26 16:01:16 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,14 +12,31 @@ def read_pair(img_pth, mask_pth):
   
 
 def create_tf_dataset(source_imgs_pth, source_masks_pth, target_imgs_pth, target_masks_pth):
+  
   ds_soucre = tf.data.Dataset.from_tensor_slices((source_imgs_pth, source_masks_pth)).shuffle(256).\
                 map(lambda img_pth, mask_pth: read_pair(img_pth, mask_pth))
   
   ds_target = tf.data.Dataset.from_tensor_slices((target_imgs_pth, target_masks_pth)).repeat().\
                map(lambda img_pth, mask_pth: read_pair(img_pth, mask_pth))
   
-  ds = tf.data.Dataset.zip((ds_source, ds_target)).batch(opt.batch_size, drop_remainder=True).prefetch(AUTOTUNE)
+  ds = tf.data.Dataset.zip((ds_source, ds_target)).shuffle(256).\
+                 batch(opt.batch_size, drop_remainder=True).prefetch(AUTOTUNE)
   return ds
+
+
+def create_dataset(opt):
+  source_imgs_pth = list(map(lambda name: opt.source_pth + '/images/' + name, os.listdir(opt.source_pth + '/images')))
+  source_masks_pth = list(map(lambda name: opt.source_pth + '/masks/' + name, os.listdir(opt.source_pth + '/masks')))
+  target_imgs_pth = list(map(lambda name: opt.target_pth + '/images/' + name, os.listdir(opt.target_pth + '/images')))
+  target_masks_pth = list(map(lambda name: opt.target_pth + '/masks/' + name, os.listdir(opt.target_pth + '/masks')))
+  pth_train, pth_val = ttp(
+    (source_imgs_pth, source_masks_pth, target_imgs_pth, target_masks_pth),
+    test_size = opt.val_size,
+    random_state = opt.seed
+  )
+  ds_train = create_tf_dataset(pth_train[0], pth_train[1], pth_train[2], pth_train[3])
+  ds_val = create_tf_dataset((pth_val[0], pth_val[1], pth_val[2], pth_val[3])
+  return ds_train, ds_val
 
 
   
