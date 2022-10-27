8d23b16f358c61df057fdf03af1caab0a89de28a
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-10-27 13:11:23 +0800

    Update train.py

--- train.py
+++ train.py
@@ -4,12 +4,15 @@ from models import AdaptFCN
 
 def parse_opt():
     parser = argparse.ArgumentParser()
+    #dataset
     parser.add_argument('--source_pth', type=str, default='../dataset/crack/source')
     parser.add_argument('--target_pth', type=str, default='../dataset/crack/target')
     parser.add_argument('--val_size', type=str, default=0.2)
     parser.add_argument('--seed', type=str, default=999)
     parser.add_argument('--batch_size', type=str, default=32)
     parser.add_argument('--img_size', type=str, default=224)
+    
+    #network
     parser.add_argument('--base', type=str, default=64)
     parser.add_argument('--num_classes', type=str, default=2)
     parser.add_argument('--num_downsamples', type=str, default=4)
