5c49cf76411812c0d621d596a253a2f403048b71
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-10-27 13:16:35 +0800

    Update and rename modules.py to losses.py

---
+++ models/losses.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+from tensorflow.keras import losses
+
+bce = losses.BinaryCrossentropy(from_logits=True)
+
+def adversarial_loss(critic_real, critic_fake, loss_tyle='logistic'):
+  if loss_type == 'logistic':
+    d_loss = bce(tf.ones_like(critic_real), critic_real) +\
+             bce(tf.zeros_like(critic_fake), critic_fake)
+    
+    g_loss = bce(tf.ones_like(critic_fake), critic_fake)
+    
+  elif loss_type == 'ls':
+    d_loss = tf.reduce_mean((1-critic_real) **2 + critic_fake ** 2)
+    g_loss = tf.reduce_mean((1-critic_fake) ** 2)
+    
+    
+  elif loss_type == 'wgan':
+    pass
+    
+  return d_loss, g_loss
