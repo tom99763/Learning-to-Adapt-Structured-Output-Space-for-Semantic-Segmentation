e92da93acd44c13c24a5d0f3ab6c3249c21f3c2b
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-10-27 14:11:05 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,12 +1,14 @@
 import argparse
 from utils import *
 from models import AdaptFCN
+from tensorflow.keras import optimizers
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    #dataset
-    parser.add_argument('--source_pth', type=str, default='../dataset/crack/source')
-    parser.add_argument('--target_pth', type=str, default='../dataset/crack/target')
+    #dataset & dirs
+    parser.add_argument('--source_pth', type=str, default='./dataset/crack/source')
+    parser.add_argument('--target_pth', type=str, default='./dataset/crack/target')
+    parser.add_argument('--ckpt_dir', type=str, default='./checkpoints')
     parser.add_argument('--val_size', type=str, default=0.2)
     parser.add_argument('--seed', type=str, default=999)
     parser.add_argument('--batch_size', type=str, default=32)
@@ -23,15 +25,29 @@ def parse_opt():
     parser.add_argument('--lambda_adv', type=str, default=1.)
     
     #optimization
-    parser.add_argument('--beta1', type=str, default=0.9)
-    parser.add_argument('--beta2', type=str, default=0.99)
+    parser.add_argument('--beta_1', type=str, default=0.9)
+    parser.add_argument('--beta_2', type=str, default=0.99)
     parser.add_argument('--lr', type=str, default=1e-4)
+    arser.add_argument('--num_epochs', type=str, default=100)
     opt, _ = parser.parse_known_args()
     return opt
 
 
 def main(opt):
     ds_train, ds_val = create_dataset(opt)
+    adaptfcn = Adaptfcn(opt)
+    adaptfcn.compile(optimizer = [
+        optimizers.Adam(learning_rate=opt.lr, beta_1=opt.beta_1, beta_2=opt.beta_2),
+        optimizers.Adam(learning_rate=opt.lr, beta_1=opt.beta_1, beta_2=opt.beta_2)
+    ])
+    callbacks = create_callbacks(opt)
+    
+    adaptfcn.fit(
+        x=ds_train,
+        validation_data=ds_val,
+        epochs=opt.num_epochs,
+        callbacks
+    )
 
 if __name__ == '__main__':
     opt = parse_opt()
