bc238fb18170b6f29ffa1a7be62819ef4c30d06a
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-10-27 16:21:58 +0800

    Update losses.py

--- models/losses.py
+++ models/losses.py
@@ -3,18 +3,20 @@ from tensorflow.keras import losses
 
 bce = losses.BinaryCrossentropy(from_logits=True)
 
-def crossentropy(y_true, y_pred):
-  return losses.SparseCategoricalCrossentropy(from_logits=True)(y_true, y_pred)
 
-def adversarial_loss(critic_real, critic_fake, loss_tyle='logistic'):
-  if loss_type == 'logistic':
-    d_loss = bce(tf.ones_like(critic_real), critic_real) +\
-             bce(tf.zeros_like(critic_fake), critic_fake)
-    
-    g_loss = bce(tf.ones_like(critic_fake), critic_fake)
-    
-  elif loss_type == 'ls':
-    d_loss = tf.reduce_mean((1-critic_real) **2 + critic_fake ** 2)
-    g_loss = tf.reduce_mean((1-critic_fake) ** 2)
-    
-  return d_loss, g_loss
+def crossentropy(y_true, y_pred):
+    return losses.SparseCategoricalCrossentropy(from_logits=True)(y_true, y_pred)
+
+
+def adversarial_loss(critic_real, critic_fake, loss_type='logistic'):
+    if loss_type == 'logistic':
+        d_loss = bce(tf.ones_like(critic_real), critic_real) + \
+                 bce(tf.zeros_like(critic_fake), critic_fake)
+
+        g_loss = bce(tf.ones_like(critic_fake), critic_fake)
+
+    elif loss_type == 'ls':
+        d_loss = tf.reduce_mean((1 - critic_real) ** 2 + critic_fake ** 2)
+        g_loss = tf.reduce_mean((1 - critic_fake) ** 2)
+
+    return d_loss, g_loss
