95e30461b18fc2d87047c9c3bfea3f624eca0645
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-10-27 13:53:31 +0800

    Update losses.py

--- models/losses.py
+++ models/losses.py
@@ -3,6 +3,9 @@ from tensorflow.keras import losses
 
 bce = losses.BinaryCrossentropy(from_logits=True)
 
+def crossentropy(y_true, y_pred):
+  return losses.SparseCategoricalCrossentropy(from_logits=True)(y_true, y_pred)
+
 def adversarial_loss(critic_real, critic_fake, loss_tyle='logistic'):
   if loss_type == 'logistic':
     d_loss = bce(tf.ones_like(critic_real), critic_real) +\
