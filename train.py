5fdde61de1c85f6e0a07dc73cac79a983522be6b
tom99763 <69715105+tom99763@users.noreply.github.com>
2022-10-27 11:20:39 +0800

    Update train.py

--- train.py
+++ train.py
@@ -3,8 +3,8 @@ from utils import *
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--source_pth', type=str, default='../input/crack-dataset/crack/source')
-    parser.add_argument('--target_pth', type=str, default='../input/crack-dataset/crack/target')
+    parser.add_argument('--source_pth', type=str, default='../dataset/crack/source')
+    parser.add_argument('--target_pth', type=str, default='../dataset/crack/target')
     parser.add_argument('--val_size', type=str, default=0.2)
     parser.add_argument('--seed', type=str, default=999)
     parser.add_argument('--batch_size', type=str, default=32)
